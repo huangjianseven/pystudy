98b46f2d8f99fe60fe8fad86b277d5c31ae71188
jianhuang <huangjian7@gmail.com>
2020-11-11 16:38:19 +0800

    The first version of exercise of 05-01

---
+++ ex_05_01.py
@@ -0,0 +1,29 @@
+#  Write a program which repeatedly reads numbers until the user enters“done”.
+#   Once “done” is entered, print out the total, count, and average of
+#  the numbers. If the user enters anything other than a number, detect their
+#  mistake using try and except and print an error message and skip to the next
+#  number.
+
+total = 0
+average = 0
+count = 0
+
+input_num = []
+
+while True:
+    num = input('Enter a number:')
+    if num == 'done':
+        break
+    else:
+        try:
+            num = float(num)
+            input_num.append(num)
+        except:
+            print("Invalid input!")
+
+for itervar in input_num:
+    total = total + itervar
+    count = count + 1
+average = total/count
+
+print(total,count,average)
